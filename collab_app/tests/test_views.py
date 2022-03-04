dd89c6847d85d38f0397cd62bd04d5529913859b
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2022-03-04 11:05:12 +0000

    Added test suite file.
    For the testing of views.

---
+++ collab_app/tests/test_views.py
@@ -0,0 +1,11 @@
+"""
+Test Views Suite
+
+Testing suite for the checking the validity and functionality of all required views.
+"""
+
+from django.test import TestCase
+from django.urls import reverse
+
+class test_calls(TestCase):
+    pass

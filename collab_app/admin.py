07142aa128b6429f2e3010d76e56a86f7e569238
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2022-03-09 12:21:45 +0000

    Added admin site access to the models.

--- collab_app/admin.py
+++ collab_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile, Forum, University, Category, Page
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ('user', 'email')
+
+class ForumAdmin(admin.ModelAdmin):
+    fields = ('name')
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Forum, ForumAdmin)
+admin.site.register(University)
+admin.site.register(Category)
+admin.site.register(Page)

1a98b31169eeb7c1df1574cb023f55c6edf16cce
Alistair Johnston <2560836j@student.gla.ac.uk>
2022-03-11 17:20:35 +0000

    Theme now uses cookies to work across pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,58 @@
+function setThemeCookie(cookieName, cookieValue, expiry){
+	const d = new Date();
+	d.setTime(d.getTime() + (expiry * 24 * 60 * 60 * 1000));
+ 	let expires = "expires="+d.toUTCString();
+ 	document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName){
+	let name = cookieName + "=";
+	let cookies = document.cookie.split(';');
+
+	for(let i = 0; i < cookies.length; i++){
+		let cookie = cookies[i];
+
+		while (cookie.charAt(0) == ' '){
+			cookie = cookie.substring(1);
+		}
+
+		if (cookie.indexOf(name) == 0){
+			return cookie.substring(name.length, cookie.length);
+		}
+
+	}
+
+	return "";
+}
+
+function validateCookie(defaultVal = true){
+	let cookie = getCookie("theme");
+	if(cookie == ""){
+		setThemeCookie("theme", defaultVal, 365);
+	}
+}
+
 function changeTheme(){
-    var element = document.body;
-    element.classList.toggle("dark-theme");
+	validateCookie();
+	let theme = getCookie("theme");
+	let element = document.body;
+
+	element.classList.toggle("dark-theme");
+
+	if(theme == "true"){
+		setThemeCookie("theme", "false", 365);
+	}
+	else{
+		setThemeCookie("theme", "true", 365);
+	}
+}
+
+function loadTheme(){
+	validateCookie();
+	let theme = getCookie("theme");
+	let element = document.body;
+	
+	if(theme == "true"){
+		element.classList.toggle("dark-theme");
+	}
 }

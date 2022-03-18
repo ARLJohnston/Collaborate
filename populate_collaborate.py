bbc607d03f44370b271b9a84c3b1c2e3c9380c69
YuenKaiTing <98586963+YuenKaiTing@users.noreply.github.com>
2022-03-18 10:28:33 +0000

    Updated populate_collaborate.py

--- populate_collaborate.py
+++ populate_collaborate.py
@@ -4,9 +4,38 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
+from collab_app import Category, Page
+
 
 def populate():
-    pass
+    university_pages = [
+        {'title': 'University Page',
+         'comment':'Welcome to university pages'}        
+        ]
+
+    categories_pages = [
+        {'title':'University',
+         'comment':'catergories'},
+         ]
+
+    forms_pages = [
+        {'title':'Forms', 'comment':'forms'}
+        ]
+
+    
+    def add_page(cat, title, url, views=0):
+        p = Page.objects.get_or_create(category=cat, title=title)[0]
+        p.url=url
+        p.views=views +1
+        p.save()
+        return p
+
+
+    # Print out the categories we have added.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'- {c}: {p}')
+
 
 if __name__ == '__main__':
     print('Starting population script...')

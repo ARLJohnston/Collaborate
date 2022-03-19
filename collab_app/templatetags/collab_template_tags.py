6b201ecd66a84e50685d7db695d7d9b46638a3de
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2022-03-19 18:54:53 +0000

    templatetags update

--- collab_app/templatetags/collab_template_tags.py
+++ collab_app/templatetags/collab_template_tags.py
@@ -4,7 +4,6 @@ from collab_app.models import Category
 register = template.Library()
 
 @register.inclusion_tag('collab_app/link_box.html')
-def links():
-    # pubs = Publisher.objects.annotate(num_books=Count('book')).order_by('-num_books')[:5]
-    # pubs[0].num_books
+def get_links():
+    """WARNING: THIS IS A TEMPORARY METHOD"""
     return {'links': Category.objects.all()[:5]}

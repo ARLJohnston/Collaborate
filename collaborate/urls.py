77ead33e9e41dcc221c4e12445cc4222dbc754a3
Holly Edees <2571943E@student.gla.ac.uk>
2022-03-23 17:35:16 +0000

    added media directory

--- collaborate/urls.py
+++ collaborate/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from collab_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('collab_app/', include('collab_app.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

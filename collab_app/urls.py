c78f644781d541373d1bd4d1c9b416d23dd71101
Holly Edees <2571943E@student.gla.ac.uk>
2022-03-27 20:08:03 +0100

    fixed urls sorry!!!

--- collab_app/urls.py
+++ collab_app/urls.py
@@ -21,8 +21,6 @@ urlpatterns = [
       path('general/<slug:category_name_slug>/', views.show_general_category,
             name='show_general_category'),
       path('general/add_general_category', views.add_general_category, name='add_general_category'),
-      
-      path('general/<slug:category_name_slug>/add_general_page', views.add_page, name='add_general_page'),
 
       path('general/<slug:category_name_slug>/<slug:page_name_slug>/',
             views.show_general_page, name='show_general_page'),
@@ -43,7 +41,7 @@ urlpatterns = [
       path('universities/<slug:university_name_slug>/<slug:category_name_slug>/',
             views.show_university_category, name='show_university_category'),
             
-      path('universities/<slug:university_name_slug>/<slug:category_name_slug>/add_university_page', views.add_page, name='add_university_page'),
+      path('universities/<slug:university_name_slug>/<slug:category_name_slug>/add_university_page', views.add_university_page, name='add_university_page'),
 
       path('universities/<slug:university_name_slug>/<slug:category_name_slug>/<slug:page_name_slug>/',
             views.show_university_page, name='show_university_page'),
